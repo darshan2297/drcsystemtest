2df855e5fa884353f96ee5d9ef78c8d9690c5115
Darshan <darshanvankawala2297@gmail.com>
2022-06-28 21:46:47 +0530

    feat(runner): OTP Module
    Feature:
    Complete The OTP Module.
    
    Running...

--- rest_app/urls.py
+++ rest_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from rest_app.views import RegisterView,LoginView
+from rest_app.views import RegisterView,LoginView,OTPView,HomeView
 
 app_name="restapp"
 
 urlpatterns = [
     path('',RegisterView.as_view(),name='register'),
     path('login',LoginView.as_view(),name='login'),
+    path('otpview',OTPView.as_view(),name='otpview'),
+    path('home',HomeView.as_view(),name='home'),
     
 ]
--- rest_app/views.py
+++ rest_app/views.py
@@ -3,16 +3,17 @@ from django.views import View
 from rest_app.models import UserProfile, EmailRecords
 from hashutils import make_pw_hash, check_pw_hash
 import pyotp
-
+from django.http import HttpResponse
 
 # Create your views here.
 
 # Create your views here.
 def generateOTP():
-    random32 = pyotp.random_base32()
-    totp = pyotp.TOTP(random32,interval=600)
+    activation_key = pyotp.random_base32()
+    totp = pyotp.TOTP(activation_key,interval=86400)
     print(totp.now())
-    return totp.now()
+    otp = totp.now()
+    return {'activation_key':activation_key,"otp":otp}
 
 class RegisterView(View):
     
@@ -39,13 +40,41 @@ class LoginView(View):
     
     def post(self, request, *args, **kwargs):
         email = request.POST['email']
+        if not (emailObj:= EmailRecords.objects.filter(email=email,is_deleted=False).first()):
+            return HttpResponse("User not found")
         
+        if getUserObj:= UserProfile.objects.filter(id=emailObj.user_id,is_deleted=False).first():
+            otpObj = generateOTP()
+            getUserObj.otp = otpObj["otp"]
+            getUserObj.activation_key = otpObj["activation_key"]
+            getUserObj.save()
+            return redirect("restapp:otpview")
+        else:
+            return HttpResponse("User not found")
+
 class OTPView(View):
     
     def get(self, request, *args, **kwargs):
-        generateOTP()
-        return render(request,'')
-        
-        
-        
-        
+        return render(request,'otpenter.html')
+    
+    def post(self, request, *args, **kwargs):
+        otp= request.POST['otp']
+        if getUserObj := UserProfile.objects.filter(otp=otp,is_deleted=False).first():
+            activation_key = getUserObj.activation_key
+            totp = pyotp.TOTP(activation_key, interval=86400)
+            print(totp)
+            verify = totp.verify(totp.now())
+            if verify:
+                user_data = {}
+                user_data['id'] = getUserObj.id
+                user_data['username'] = getUserObj.username
+                request.session['user'] = user_data
+                return redirect("restapp:home")
+        else:
+           return HttpResponse("User not found") 
+
+class HomeView(View):
+    
+    def get(self, request, *args, **kwargs):
+        userData = request.session.get('user')['username']
+        return HttpResponse(f"Home Page {userData}")
